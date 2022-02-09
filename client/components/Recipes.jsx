eb6ddb7a82890e4cbdb13ea037425ecdc76a7731
Evelyn Tan <evetan.wy@gmail.com>
2022-02-09 16:01:30 +1300

    API working with query params

--- client/components/Recipes.jsx
+++ client/components/Recipes.jsx
@@ -1,28 +1,59 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 import { getRecipes } from "../api";
 
 function Recipes() {
   const [recipe, setRecipe] = useState("");
+  const [search, setSearch] = useState("");
+
+  // recipe.hits[0].recipe.label - Food title
+  // recipe.hits[0].recipe.url - Link to page
+  // recipe.hits[0].recipe.image - image
 
   const handleClick = () => {
-    getRecipes()
+    getRecipes(search)
       .then((recipe) => {
-        setRecipe(recipe.hits[0].recipe.url);
+        setRecipe(recipe.hits[0].recipe.image);
         return null;
       })
       .catch((err) => {
         console.log(err.message);
       });
+    setSearch("");
+  };
+
+  const handleChange = (evt) => {
+    console.log(evt.target.value);
+    // const selected = evt.target.value;
+    setSearch(evt.target.value);
+    //alert(`${evt.target.value}`);
+    evt.preventDefault();
   };
 
   return (
     <>
+      <Form.Group controlId="foodSelection">
+        <Form.Control type="text" placeholder="Normal text" />
+        <Form.Label className="form-wording">Find the food</Form.Label>
+        {/* <select
+          className="form-select form-select-lg mb-3 mt-2"
+          id="dropdown-button-dark-example2"
+          aria-label=".form-select-lg example"
+          onChange={handleChange}
+        > */}
+        {/* <option defaultChecked="chicken">Please select the food</option>
+        <option value="Chicken">Chicken</option>
+        <option value="Pizza">Pizza</option>
+        <option value="Pasta">Pasta</option> */}
+        {/* </select> */}
+        <input type="text" value={search} onChange={handleChange} />
+        {/* <button type="submit">Fill ma belly</button> */}
+        <button onClick={handleClick}>Fill ma belly</button>
+      </Form.Group>
       <div>
         {recipe}
-        <p>Testing</p>
         <br />
-        <button onClick={handleClick}>Fill ma belly</button>
       </div>
     </>
   );
--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -8,14 +8,14 @@ const router = express.Router();
 // API key is kept secret in .env file
 // check index.js for configuration
 
-router.get("/", (req, res) => {
+router.get("/:query", (req, res) => {
   return request
     .get(
       // `https://api.edamam.com/api/recipes/v2?api_id=${process.env.API_ID}&api_key=${process.env.API_KEY}&q=pizza&type=public`
-      `https://api.edamam.com/api/recipes/v2?app_id=ccec1d0d&app_key=2d0793fa483cb29f43947eb7866626f9&q=chicken&type=public`
+      // `https://api.edamam.com/api/recipes/v2?app_id=ccec1d0d&app_key=2d0793fa483cb29f43947eb7866626f9&type=public&q=chicken`
+      `https://api.edamam.com/api/recipes/v2?app_id=ccec1d0d&app_key=2d0793fa483cb29f43947eb7866626f9&type=public&q=${req.params.query}`
     ) // anything that needs to parse through env needs to have a process
     .then((response) => {
-      //   console.log("routeTest", response.body);
       res.json(response.body);
       return null;
     })
